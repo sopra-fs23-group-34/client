d980141f296fbfc5970b1dba3fc9964eb22490a3
sugar-free55 <andre.seidenglanz@gmail.com>
2023-03-29 16:31:57 +0200

    first draft of change Password view with the edit function

---
+++ src/components/views/EditPassword.js
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+import {api, handleError} from 'helpers/api';
+import {useHistory, useParams} from 'react-router-dom';
+import {Button} from 'components/ui/Button';
+import 'styles/views/Login.scss';
+import BaseContainer from "components/ui/BaseContainer";
+import PropTypes from "prop-types";
+
+/*
+It is possible to add multiple components inside a single file,
+however be sure not to clutter your files with an endless amount!
+As a rule of thumb, use one file per component and only add small,
+specific components that belong to the main one and the same file. :)
+ */
+const FormField = props => {
+  return (
+    <div className="login field">
+      <label className="login label">
+        {props.label}
+      </label>
+      <input
+        className="login input"
+        placeholder="enter here.."
+        value={props.value}
+        onChange={e => props.onChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+FormField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+const EditPassword = () => {
+  const history = useHistory();
+  const {id} = useParams();
+  const [oldPassword, setOldPassword] = useState(null);
+  const [newPassword, setNewPassword] = useState(null);
+  const [newRepeatPassword, setNewRepeatPassword] = useState(null);
+
+
+
+  const saveChanges = async () => {
+    try {
+      const requestBody = JSON.stringify({oldPassword, newPassword});
+      await api.put('/users/update' +id, requestBody);
+// Login successfully worked --> navigate to the route /game in the GameRouter
+      history.push(`/hub`);
+    } catch (error) {
+      alert(`Something went wrong during the editing: \n${handleError(error)}`);
+    }
+  };
+
+  return (
+    <BaseContainer>
+      <div className="login container">
+        <div className="login form">
+          <FormField
+            label="old password"
+            value={oldPassword}
+            onChange={n => setOldPassword(n)}
+          />
+          <FormField
+              label="new password"
+              value={newPassword}
+              onChange={n => setNewPassword(n)}
+          />
+          <FormField
+              label="repeat new password"
+              value={newRepeatPassword}
+              onChange={n => setNewRepeatPassword(n)}
+          />
+          <div className="login button-container">
+            <Button
+              disabled={!oldPassword && !newPassword}
+              width="100%"
+              onClick={() => saveChanges()}
+            >
+              save changes
+            </Button>
+          </div>
+
+        </div>
+      </div>
+    </BaseContainer>
+  );
+};
+
+/**
+ * You can get access to the history object's properties via the withRouter.
+ * withRouter will pass updated match, location, and history props to the wrapped component whenever it renders.
+ */
+export default EditPassword;
