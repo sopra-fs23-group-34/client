895cd77dab6888b1aad817267365528507b0ac86
Nico Manzoni <nico@menzipics.ch>
2023-04-05 14:48:38 +0200

    WIP

--- src/components/views/WebsocketViews/WebsocketWrapper.js
+++ src/components/views/WebsocketViews/WebsocketWrapper.js
@@ -7,14 +7,15 @@ export const WebsocketWrapper = React.createContext();
 export function WebsocketWrapperComponent({children}) {
   const [ref, setRef] = useState(null);
   const [msg, setMsg] = useState(null);
-  
+  const [gameCode, setGameCode] = useState(null);
+  const [userid, setuserid] = useState(null);
 
   const handleMessage = (msg) => {
     setMsg(msg);
   };
 
   return (
-    <WebsocketWrapper.Provider value={{ ref , msg }}>
+    <WebsocketWrapper.Provider value={{ ref , msg, gameCode, userid }}>
       <SockJsClient
         url='http://localhost:8080/ws'
         topics={['/topic/lobbies/' + "" , '/topic/players/' + ""]}
