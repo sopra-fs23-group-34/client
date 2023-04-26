a2e39088dc3f0d354829be6cc56f0d8552db67d3
Nico Manzoni <nico@menzipics.ch>
2023-04-26 11:01:59 +0200

    small ui overhaul #42

--- src/components/ui/Item.js
+++ src/components/ui/Item.js
@@ -2,7 +2,7 @@ import { styled } from '@mui/material';
 import Paper from '@mui/material/Paper';
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : "#262632",
+    backgroundColor: theme.palette.mode === 'dark' ? '#ffffff' : "#3b3b37",
     ...theme.typography.body2,
     boxShadow: "none",
     padding: theme.spacing(1),
