f376ef9bbf5190496c106e1c5bae9fadc4310d0a
sugar-free55 <andre.seidenglanz@gmail.com>
2023-04-27 13:24:55 +0200

    Added a title Close #2, Close #1, Close #34 Close #2, Close #35, Close #39

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -74,6 +74,7 @@ const Register = () => {
         <BaseContainer>
             <div className="register container">
                 <div className="register form">
+                    <h1 style={{marginTop: 0}}>Register Form</h1>
                     <FormField
                         label="Username"
                         value={username}
